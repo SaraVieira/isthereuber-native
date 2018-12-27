85ef0299f707431abc6494e44929a7d3f1065565
Sara Vieira <hey@iamsaravieira.com>
2018-12-27 15:41:59 +0000

    move loading

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import {
-  Image,
-  Linking,
-} from 'react-native'
+import { Image, Linking, ActivityIndicator } from 'react-native'
 import logo from '../../assets/logo.png'
 import AlgoliaLogo from '../../assets/algolia.png'
 import {
@@ -13,31 +10,71 @@ import {
   Logo,
   Algolia
 } from './Home.elements'
+import { fixNameB } from '../utils/fixName'
+import { search } from '../utils/algolia'
 
 export default class Home extends React.Component {
-  state = { text: '' }
+  state = { text: '', loading: false, cities: [] }
 
   static navigationOptions = {
     title: 'Home',
     header: null
   }
 
+  search = () => {
+    this.setState({
+      loading: true
+    })
+    search(this.state.text, (err, content) => {
+      if (err) {
+        this.setState({
+          loading: false
+        })
+        console.error(err)
+        return
+      }
+
+      this.setState(
+        {
+          cities: content.hits,
+          loading: false
+        },
+        this.checkCitiesLength
+      )
+    })
+  }
+
+  checkCitiesLength = () => {
+    const cities = this.state.cities
+    if (cities.length === 1) {
+      this.props.navigation.push('City', {
+        city: cities[0]
+      })
+    } else {
+      this.props.navigation.push('Search', {
+        cities: cities
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timer) clearTimeout(this.timer)
+  }
+
   render() {
+    const { loading } = this.state
     return (
       <Wrapper>
         <Logo source={logo} />
         <Label>Is there Uber in</Label>
         <InputWrapper>
           <Input
-            onSubmitEditing={() =>
-              this.props.navigation.push('Search', {
-                city: this.state.text
-              })
-            }
+            onSubmitEditing={this.search}
             onChangeText={text => this.setState({ text })}
           />
           <Label style={{ marginLeft: 10 }}>?</Label>
         </InputWrapper>
+        {loading ? <ActivityIndicator size="large" color="#e8eaf6" /> : null}
         <Algolia
           title="Search by Algolia"
           onPress={() => Linking.openURL('https://www.algolia.com')}
--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Text, TouchableOpacity, ActivityIndicator } from 'react-native'
-import { fixNameB } from '../utils/fixName'
-import { search } from '../utils/algolia'
 import Flag from 'react-native-flags'
 import { Wrapper, Subtitle, Cities, List, City } from './Search.elements'
 
@@ -10,100 +8,43 @@ export default class Search extends React.Component {
     title: 'Search'
   }
 
-  state = {
-    cities: [],
-    loaded: false
-  }
-
-  search = city => {
-    search(city, (err, content) => {
-      if (err) {
-        this.setState({
-          loaded: true
-        })
-        console.error(err)
-        return
-      }
-
-      this.setState(
-        {
-          cities: content.hits
-        },
-        this.checkCitiesLength
-      )
-    })
-  }
-
-  checkCitiesLength = () => {
-    const cities = this.state.cities
-    if (cities.length === 1) {
-      this.props.navigation.push('City', {
-        city: cities[0]
-      })
-    }
-
-    this.timer = setTimeout(
-      () =>
-        this.setState({
-          loaded: true
-        }),
-      600
-    )
-  }
-
-  componentWillUnmount() {
-    if (this.timer) clearTimeout(this.timer)
-  }
-
-  componentDidMount() {
-    const city = this.props.navigation.state.params.city
-    this.search(city)
-  }
-
   render() {
-    const { loaded, cities } = this.state
-    const city = this.props.navigation.state.params.city
+    const cities = this.props.navigation.state.params.cities
 
     return (
       <Wrapper>
-        {!loaded ? (
-          <ActivityIndicator size="large" color="#e8eaf6" />
-        ) : (
-          <>
-            <Subtitle>More than one city matches your search</Subtitle>
-            <Subtitle>What city did you mean ?</Subtitle>
-            <Cities>
-              {cities.map(city => (
-                <List key={city.info.place_id}>
-                  <TouchableOpacity
-                    onPress={() =>
-                      this.props.navigation.push('City', {
-                        city
-                      })
-                    }
-                  >
-                    <Text
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                      }}
-                    >
-                      <City style={{ marginRight: 20 }}> {city.name}</City>
+        <Subtitle>More than one city matches your search</Subtitle>
+        <Subtitle>What city did you mean ?</Subtitle>
+        <Cities>
+          {cities.map(city => (
+            <List key={city.info.place_id}>
+              <TouchableOpacity
+                onPress={() =>
+                  this.props.navigation.push('City', {
+                    city
+                  })
+                }
+              >
+                <Text
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                  }}
+                >
+                  <City style={{ marginRight: 20 }}> {city.name}</City>
 
-                      <Flag
-                        type="flat"
-                        code={city.info.country.short_name}
-                        size={32}
-                      />
-                    </Text>
-                  </TouchableOpacity>
-                </List>
-              ))}
-            </Cities>
-          </>
-        )}
+                  <Flag
+                    type="flat"
+                    code={city.info.country.short_name}
+                    size={32}
+                  />
+                </Text>
+              </TouchableOpacity>
+            </List>
+          ))}
+        </Cities>
       </Wrapper>
     )
   }
